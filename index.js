3f316c08b9169bb309dff4d34605a0149985f2f0
Jaya Ramkumar <jayaramkumar@MacBook-Pro.fios-router.home>
2023-05-22 20:23:37 -0400

    homework

--- index.js
+++ index.js
@@ -13,3 +13,104 @@ move(newImage('assets/well.png')).to(500, 575)
 move(newItem('assets/sword.png')).to(500, 555)
 move(newItem('assets/shield.png')).to(165, 335)
 move(newItem('assets/staff.png')).to(600, 250)
+
+
+// const character = newImage('assets/green-character/static.gif')
+// let direction = null;
+// let x = 100;
+// let y = 250;
+
+// const character = newImage('assets/green-character/static.gif')
+// let direction = null;
+// let x = 100;
+// let y = 250;
+// move(character).to(100, 250)
+
+// let direction = null;
+// let x = 100;
+// let y = 250;
+
+// if(direction === 'west'){
+//     x = x - 1
+// }
+
+// let direction = null;
+// let x = 100;
+// let y = 250;
+
+// if(direction === 'west'){
+//     x = x - 1
+// }
+// if(direction === 'north'){
+//     y = y + 1
+// }
+// if(direction === 'east'){
+//     x = x + 1
+// }
+// if(direction === 'south'){
+//     y = y - 1
+// }
+// character.style.left = x + 'px'
+// character.style.bottom = y + 'px'
+
+
+// const character = newImage('assets/green-character/static.gif')
+// let direction = null;
+// let x = 100;
+// let y = 250;
+
+// function moveCharacter(){ 
+//     if(direction === 'west'){
+//         x = x - 1
+//     }
+//     if(direction === 'north'){
+//         y = y + 1
+//     }
+//     if(direction === 'east'){
+//         x = x + 1
+//     }
+//     if(direction === 'south'){
+//         y = y - 1
+//     }
+//     character.style.left = x + 'px'
+//     character.style.bottom = y + 'px'
+// }
+
+// // function moveCharacter(){ 
+// //     if(direction === 'west'){
+// //         x = x - 1
+// //     }
+// //     if(direction === 'north'){
+// //         y = y + 1
+// //     }
+// //     if(direction === 'east'){
+// //         x = x + 1
+// //     }
+// //     if(direction === 'south'){
+// //         y = y - 1
+// //     }
+// //     character.style.left = x + 'px'
+// //     character.style.bottom = y + 'px'
+// // }
+// // setInterval(moveCharacter, 1)                                    
+// // direction = 'east'
+
+
+// setInterval(function(){ 
+//     if(direction === 'west'){
+//         x = x - 1
+//     }
+//     if(direction === 'north'){
+//         y = y + 1
+//     }
+//     if(direction === 'east'){
+//         x = x + 1
+//     }
+//     if(direction === 'south'){
+//         y = y - 1
+//     }
+//     character.style.left = x + 'px'
+//     character.style.bottom = y + 'px'
+// }, 1)
+
+setInterval(moveCharacter, 1)                                    
